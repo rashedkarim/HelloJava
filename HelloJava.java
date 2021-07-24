3d49a4f36302a906571d8d701447f6d9c0b36bf5
Rashed Karim <rashedmkarim@gmail.com>
2021-07-24 13:23:33 +0800

    add Student.java

--- HelloJava.java
+++ HelloJava.java
@@ -1,8 +1,14 @@
+import lib.Student;
+
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello Java World!!");
-        for(int i = 0; i<10; i++){
+        /* for(int i = 0; i<10; i++){
             System.out.println("SL - "+i);
-        }
+        } */
+
+        Student student = new Student(1, "Golam");
+
+        System.out.println(student.toString());
     }
 }
