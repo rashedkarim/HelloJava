018098dc8d90377c55e4a9c3930d12cb26f7223f
Rashed Karim <rashedmkarim@gmail.com>
2021-07-24 13:08:43 +0800

    add HelloJava.java

---
+++ HelloJava.java
@@ -0,0 +1,8 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello Java World!!");
+        for(int i = 0; i<10; i++){
+            System.out.println("SL - "+i);
+        }
+    }
+}
